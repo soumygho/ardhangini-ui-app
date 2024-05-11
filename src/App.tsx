cd91f3f76affa62982f89c60c7f4428e5ba11281
Soumyakanti Ghosh <soumyaghosh.ghosh@gmail.com>
2024-05-11 13:20:09 +0530

    Github pages changes.

--- src/App.tsx
+++ src/App.tsx
@@ -6,16 +6,17 @@ import "./css/style-new.css";
 import Header from "./components/header";
 import Footer from "./components/footer";
 import Body from "./components/body";
-import { BrowserRouter } from "react-router-dom";
+//import { BrowserRouter as Router } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 
 function App() {
   return (
     <>
-      <BrowserRouter>
+      <Router>
         <Header></Header>
         <Body></Body>
         <Footer></Footer>
-      </BrowserRouter>
+      </Router>
     </>
   );
 }
